ead091cf9762c1965470a25fb80658b67cf21ed6
hyejun8701 <hyejun8701@gmail.com>
2019-08-27 20:37:02 +0900

    static 메서드(정적), factory 메서드(인스턴스), AbstractFactoryBean<T> 이용해보기

--- src/main/java/com/apress/springrecipes/shop/Main.java
+++ src/main/java/com/apress/springrecipes/shop/Main.java
@@ -1,6 +1,8 @@
 package com.apress.springrecipes.shop;
 
 import com.apress.springrecipes.shop.config.ShopConfiguration;
+import com.apress.springrecipes.shop.domain.Battery;
+import com.apress.springrecipes.shop.domain.Disc;
 import com.apress.springrecipes.shop.domain.Product;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,27 +14,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(ShopConfiguration.class);
 
-        String alert = context.getMessage("alert.checkout", null, Locale.US);
-        String alert_inventory = context.getMessage("alert.inventory.checkout", new Object[]{"DVD-RW 3.0", new Date()}, Locale.US);
+//        String alert = context.getMessage("alert.checkout", null, Locale.US);
+//        String alert_inventory = context.getMessage("alert.inventory.checkout", new Object[]{"DVD-RW 3.0", new Date()}, Locale.US);
+//
+//        System.out.println("The I18N message for alert.checkout is: " + alert);
+//        System.out.println("The I18N message for alert.inventory.checkout is: " + alert_inventory);
 
-        System.out.println("The I18N message for alert.checkout is: " + alert);
-        System.out.println("The I18N message for alert.inventory.checkout is: " + alert_inventory);
+//        Product aaa = context.getBean("aaa", Product.class);
+//        Product cdrw = context.getBean("cdrw", Product.class);
+//        Product dvdrw = context.getBean("dvdrw", Product.class);
+//
+//        ShoppingCart cart1 = context.getBean(ShoppingCart.class);
+//        cart1.addItem(aaa);
+//        cart1.addItem(cdrw);
+//
+//        System.out.println("Shopping Cart 1 contains " + cart1.getItems());
 
-        Product aaa = context.getBean("aaa", Product.class);
-        Product cdrw = context.getBean("cdrw", Product.class);
-        Product dvdrw = context.getBean("dvdrw", Product.class);
-
-        ShoppingCart cart1 = context.getBean(ShoppingCart.class);
-        cart1.addItem(aaa);
-        cart1.addItem(cdrw);
-
-        System.out.println("Shopping Cart 1 contains " + cart1.getItems());
-
-        Cashier2_7 cashier2_7 = context.getBean(Cashier2_7.class);
-        cashier2_7.checkout(cart1);
-
-        Cashier2_8 cashier2_8 = context.getBean(Cashier2_8.class);
-        cashier2_8.checkout(cart1);
+//        Cashier2_7 cashier2_7 = context.getBean(Cashier2_7.class);
+//        cashier2_7.checkout(cart1);
+//
+//        Cashier2_8 cashier2_8 = context.getBean(Cashier2_8.class);
+//        cashier2_8.checkout(cart1);
 
 //
 //        ShoppingCart cart2 = context.getBean(ShoppingCart.class);
@@ -45,5 +47,14 @@ public class Main {
 //
 //        System.out.println("And don't forgot our discounts!");
 //        System.out.println(props);
+
+        Battery aaa = context.getBean("discountFactoryBeanAAA", Battery.class);
+        System.out.println(aaa);
+
+        Disc cdrw = context.getBean("discountFactoryBeanCDRW", Disc.class);
+        System.out.println(cdrw);
+
+        Disc dvdrw = context.getBean("discountFactoryBeanDVDRW", Disc.class);
+        System.out.println(dvdrw);
     }
 }
--- src/main/java/com/apress/springrecipes/shop/config/ShopConfiguration.java
+++ src/main/java/com/apress/springrecipes/shop/config/ShopConfiguration.java
@@ -2,6 +2,8 @@ package com.apress.springrecipes.shop.config;
 
 import com.apress.springrecipes.shop.BannerLoader;
 import com.apress.springrecipes.shop.Cashier2_8;
+import com.apress.springrecipes.shop.DiscountFactoryBean;
+import com.apress.springrecipes.shop.ProductCreator;
 import com.apress.springrecipes.shop.domain.Battery;
 import com.apress.springrecipes.shop.domain.Disc;
 import com.apress.springrecipes.shop.domain.Product;
@@ -14,59 +16,64 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.io.Resource;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 @PropertySource("classpath:discounts.properties")
 @ComponentScan("com.apress.springrecipes.shop")
 public class ShopConfiguration {
-    @Value("${endofyear.discount:0}")
-    private double specialEndofyearDiscountField;
+//    @Value("${endofyear.discount:0}")
+//    private double specialEndofyearDiscountField;
+//
+//    @Value("classpath:banner.txt")
+//    private Resource banner;
+//
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//        return new PropertySourcesPlaceholderConfigurer();
+//    }
+//
+//    @Bean
+//    public ReloadableResourceBundleMessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasenames("classpath:messages");
+//        messageSource.setCacheSeconds(1);
+//        return messageSource;
+//    }
+//
+//    @Bean
+//    public BannerLoader bannerLoader() {
+//        BannerLoader bl = new BannerLoader();
+//        bl.setBanner(banner);
+//        return bl;
+//    }
 
-    @Value("classpath:banner.txt")
-    private Resource banner;
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public ReloadableResourceBundleMessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasenames("classpath:messages");
-        messageSource.setCacheSeconds(1);
-        return messageSource;
-    }
-
-    @Bean
-    public BannerLoader bannerLoader() {
-        BannerLoader bl = new BannerLoader();
-        bl.setBanner(banner);
-        return bl;
+    /*@Bean
+    public ProductCreator productCreatorFactory() {
+        ProductCreator factory = new ProductCreator();
+        Map<String, Product> products = new HashMap<>();
+        products.put("aaa", new Battery("AAA", 2.5));
+        products.put("cdrw", new Disc("CD-RW", 1.5));
+        products.put("dvdrw", new Disc("DVD-RW", 3.0));
+        factory.setProducts(products);
+        return factory;
     }
 
     @Bean
     public Product aaa() {
-        Battery p1 = new Battery("AAA", 2.5);
-        p1.setRechargeable(true);
-
-        System.out.println(specialEndofyearDiscountField);
-
-        return p1;
+        return productCreatorFactory().createProduct("aaa");
     }
 
     @Bean
     public Product cdrw() {
-        Disc p2 = new Disc("CD-RW", 1.5);
-        p2.setCapacity(700);
-        return p2;
+        return productCreatorFactory().createProduct("cdrw");
     }
 
     @Bean
     public Product dvdrw() {
-        Disc p2 = new Disc("DVD-RW", 3.0);
-        p2.setCapacity(700);
-        return p2;
-    }
+        return productCreatorFactory().createProduct("dvdrw");
+    }*/
 
 //    @Bean(initMethod = "openFile", destroyMethod = "closeFile")
 //    public Cashier2_8 cashier() {
@@ -76,4 +83,46 @@ public class ShopConfiguration {
 //        c1.setPath(path);
 //        return c1;
 //    }
+
+    @Bean
+    public Product aaa() {
+        Battery aaa = new Battery("AAA", 2.5);
+        return aaa;
+    }
+
+    @Bean
+    public Product cdrw() {
+        Disc cdrw = new Disc("CD-RW", 1.5);
+        return cdrw;
+    }
+
+    @Bean
+    public Product dvdrw() {
+        Disc dvdrw = new Disc("DVD-RW", 3.0);
+        return dvdrw;
+    }
+
+    @Bean
+    public DiscountFactoryBean discountFactoryBeanAAA() {
+        DiscountFactoryBean factory = new DiscountFactoryBean();
+        factory.setProduct(aaa());
+        factory.setDiscount(0.2);
+        return factory;
+    }
+
+    @Bean
+    public DiscountFactoryBean discountFactoryBeanCDRW() {
+        DiscountFactoryBean factory = new DiscountFactoryBean();
+        factory.setProduct(cdrw());
+        factory.setDiscount(0.1);
+        return factory;
+    }
+
+    @Bean
+    public DiscountFactoryBean discountFactoryBeanDVDRW() {
+        DiscountFactoryBean factory = new DiscountFactoryBean();
+        factory.setProduct(dvdrw());
+        factory.setDiscount(0.1);
+        return factory;
+    }
 }
