d996f27d1bbfaa34f2623c6fd77be2bde56d4b4e
hyejun8701 <hyejun8701@gmail.com>
2019-08-27 11:13:31 +0900

    resource

--- src/main/java/com/apress/springrecipes/shop/Main.java
+++ src/main/java/com/apress/springrecipes/shop/Main.java
@@ -2,17 +2,49 @@ package com.apress.springrecipes.shop;
 
 import com.apress.springrecipes.shop.config.ShopConfiguration;
 import com.apress.springrecipes.shop.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(ShopConfiguration.class);
+
+        String alert = context.getMessage("alert.checkout", null, Locale.US);
+        String alert_inventory = context.getMessage("alert.inventory.checkout", new Object[]{"DVD-RW 3.0", new Date()}, Locale.US);
+
+        System.out.println("The I18N message for alert.checkout is: " + alert);
+        System.out.println("The I18N message for alert.inventory.checkout is: " + alert_inventory);
 
         Product aaa = context.getBean("aaa", Product.class);
         Product cdrw = context.getBean("cdrw", Product.class);
+        Product dvdrw = context.getBean("dvdrw", Product.class);
 
-        System.out.println(aaa);
-        System.out.println(cdrw);
+        ShoppingCart cart1 = context.getBean(ShoppingCart.class);
+        cart1.addItem(aaa);
+        cart1.addItem(cdrw);
+
+        System.out.println("Shopping Cart 1 contains " + cart1.getItems());
+
+        Cashier cashier = context.getBean(Cashier.class);
+        cashier.checkout(cart1);
+//
+//        ShoppingCart cart2 = context.getBean(ShoppingCart.class);
+//        cart2.addItem(dvdrw);
+//
+//        System.out.println("Shopping Cart 2 contains " + cart2.getItems());
+//
+//        Resource resource = new ClassPathResource("discounts.properties");
+//        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+//
+//        System.out.println("And don't forgot our discounts!");
+//        System.out.println(props);
     }
 }
--- src/main/java/com/apress/springrecipes/shop/config/ShopConfiguration.java
+++ src/main/java/com/apress/springrecipes/shop/config/ShopConfiguration.java
@@ -1,23 +1,68 @@
 package com.apress.springrecipes.shop.config;
 
+import com.apress.springrecipes.shop.BannerLoader;
 import com.apress.springrecipes.shop.domain.Battery;
 import com.apress.springrecipes.shop.domain.Disc;
 import com.apress.springrecipes.shop.domain.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.Resource;
 
 @Configuration
+@PropertySource("classpath:discounts.properties")
+@ComponentScan("com.apress.springrecipes.shop")
 public class ShopConfiguration {
+    @Value("${endofyear.discount:0}")
+    private double specialEndofyearDiscountField;
+
+    @Value("classpath:banner.txt")
+    private Resource banner;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:messages");
+        messageSource.setCacheSeconds(1);
+        return messageSource;
+    }
+
+    @Bean
+    public BannerLoader bannerLoader() {
+        BannerLoader bl = new BannerLoader();
+        bl.setBanner(banner);
+        return bl;
+    }
+
     @Bean
     public Product aaa() {
         Battery p1 = new Battery("AAA", 2.5);
         p1.setRechargeable(true);
+
+        System.out.println(specialEndofyearDiscountField);
+
         return p1;
     }
 
     @Bean
     public Product cdrw() {
         Disc p2 = new Disc("CD-RW", 1.5);
+        p2.setCapacity(700);
+        return p2;
+    }
+
+    @Bean
+    public Product dvdrw() {
+        Disc p2 = new Disc("DVD-RW", 3.0);
         p2.setCapacity(700);
         return p2;
     }
