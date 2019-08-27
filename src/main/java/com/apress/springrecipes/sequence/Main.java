422e74dff38bcbc791d771f4667302003be6d4bb
hyejun8701 <hyejun8701@gmail.com>
2019-08-27 16:16:13 +0900

    @Configuration 에서 RequiredAnnotationBeanPostProcessor 빈으로 설정하고, 또한 @Configuration 에서는 shouldSkip 이 무조건 true 로 작동하는 부분을 재정의하여 @Required 로 프로퍼티 감시하기

--- src/main/java/com/apress/springrecipes/sequence/Main.java
+++ src/main/java/com/apress/springrecipes/sequence/Main.java
@@ -4,23 +4,16 @@ import com.apress.springrecipes.sequence.config.SequenceConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.apress.springrecipes.sequence.repository.SequenceDao;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(SequenceConfiguration.class);
-//
-//        SequenceGenerator sequenceGenerator = context.getBean(SequenceGenerator.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(SequenceConfiguration.class);
 
-        ApplicationContext context =
-                new GenericXmlApplicationContext("appContext.xml");
-
-        SequenceGenerator generator =
-                (SequenceGenerator) context.getBean("sequenceGenerator");
+        SequenceGenerator sequenceGenerator = context.getBean(SequenceGenerator.class);
 
         //System.out.println(sequenceDao.getNextValue("IT"));
         //System.out.println(sequenceDao.getNextValue("IT"));
-        System.out.println(generator.getSequence());
-        System.out.println(generator.getSequence());
+        System.out.println(sequenceGenerator.getSequence());
+        System.out.println(sequenceGenerator.getSequence());
     }
 }
--- src/main/java/com/apress/springrecipes/sequence/config/SequenceConfiguration.java
+++ src/main/java/com/apress/springrecipes/sequence/config/SequenceConfiguration.java
@@ -3,26 +3,41 @@ package com.apress.springrecipes.sequence.config;
 import com.apress.springrecipes.sequence.DatePrefixGenerator;
 import com.apress.springrecipes.sequence.SequenceGenerator;
 import org.springframework.beans.factory.annotation.RequiredAnnotationBeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class SequenceConfiguration {
-//    @Bean
-//    public DatePrefixGenerator datePrefixGenerator() {
-//        DatePrefixGenerator dpg = new DatePrefixGenerator();
-//        dpg.setPattern("yyyyMMdd");
-//        return dpg;
-//    }
-//
-//    @Bean
-//    @DependsOn("datePrefixGenerator")
-//    public SequenceGenerator sequenceGenerator() {
-//        SequenceGenerator sequence = new SequenceGenerator();
-//        sequence.setInitial(100000);
-//        //sequence.setSuffix("A");
-//        //sequence.setPrefixGenerator(datePrefixGenerator());
-//        return sequence;
-//    }
+    @Bean
+    public DatePrefixGenerator datePrefixGenerator() {
+        DatePrefixGenerator dpg = new DatePrefixGenerator();
+        dpg.setPattern("yyyyMMdd");
+        return dpg;
+    }
+
+    @Bean
+    @DependsOn("datePrefixGenerator")
+    public SequenceGenerator sequenceGenerator() {
+        SequenceGenerator sequence = new SequenceGenerator();
+        sequence.setInitial(100000);
+        //sequence.setSuffix("A");
+        //sequence.setPrefixGenerator(datePrefixGenerator());
+        return sequence;
+    }
+
+    @Bean
+    public RequiredAnnotationBeanPostProcessor processor () {
+        return new RequiredAnnotationBeanPostProcessor() {
+            @Override
+            protected boolean shouldSkip (ConfigurableListableBeanFactory beanFactory,
+                                          String beanName) {
+                if (beanName.equals("sequenceGenerator")) {
+                    return false;
+                }
+                return super.shouldSkip(beanFactory, beanName);
+            }
+        };
+    }
 }
