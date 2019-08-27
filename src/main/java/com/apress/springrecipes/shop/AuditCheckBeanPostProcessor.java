1dfe3d515c75ad89773089a41b3da5e414ecc870
hyejun8701 <hyejun8701@gmail.com>
2019-08-27 14:19:47 +0900

    빈 후처리기 특정 빈만 적용

--- src/main/java/com/apress/springrecipes/shop/AuditCheckBeanPostProcessor.java
+++ src/main/java/com/apress/springrecipes/shop/AuditCheckBeanPostProcessor.java
@@ -1,5 +1,6 @@
 package com.apress.springrecipes.shop;
 
+import com.apress.springrecipes.shop.domain.Product;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,21 @@ public class AuditCheckBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("In AuditCheckBeanPostProcessor.postProcessBeforeInitialization, " + "processing bean type: " + bean.getClass());
+        if(bean instanceof Product) {
+            String productName = ((Product) bean).getName();
+            System.out.println("In AuditCheckBeanPostProcessor.postProcessBeforeInitialization, "
+                    + "processing Product: " + productName);
+        }
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof Product) {
+            String productName = ((Product) bean).getName();
+            System.out.println("In AuditCheckBeanPostProcessor.postProcessAfterInitialization, "
+                    + "processing Product: " + productName);
+        }
         return bean;
     }
 }
