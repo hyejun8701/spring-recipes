757636c69f7a5d0de8bec82fc6e38f6709fce695
hyejun8701 <hyejun8701@gmail.com>
2019-08-28 15:37:48 +0900

    JoinPoint 선언하여 메서드명, 인수값 등 정보 조회하기 @Before("execution(* *.*(..))") 에서 첫번째 *은 접근제어자, 두번째 * 은 클래스명, .*은 메서드명, (..)은 인수갯수와 상관없이 모든 것을 나타낸다.

--- src/main/java/com/apress/springrecipes/calculator/CalculatorLoggingAspect.java
+++ src/main/java/com/apress/springrecipes/calculator/CalculatorLoggingAspect.java
@@ -2,17 +2,21 @@ package com.apress.springrecipes.calculator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class CalculatorLoggingAspect {
     private Log log = LogFactory.getLog(this.getClass());
 
-    @Before("execution(* ArithmeticCalculator.add(..))")
-    public void logBefore() {
-        log.info("The method add() begins");
+    @Before("execution(* *.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("The method " + joinPoint.getSignature().getName()
+        + "() begins with " + Arrays.toString(joinPoint.getArgs()));
     }
 }
