a8f29fdb0e3182f16d36b56d04af704d143fa2f1
hyejun8701 <hyejun8701@gmail.com>
2019-08-29 22:26:24 +0900

    위빙 - 나중에 다시 복습 <context:load-time-weaver/> 과 @EnableLoadTimeWeaving 적용이 둘다 정상적으로 실행이 안되었음. 일단 패스., AspectJ AOP 적용시 문제점.   Spring AOP / 프로그래밍                 2013. 7. 4. 13:59                 복사http://blog.naver.com/goddes4/30171234653                 번역하기
                    전용뷰어 보기
                    aspectjweaver-1.7.2 를 이용하지 않고 spring-instrument-3.2.3.RELEASE 를 이용할 때
                    스프링의 LTW 초기화가 이루어 지기 전에 대상 도메인 객체를 먼저 생성 시키면
                    LTW 초기화 이후에 도메인 객체 생성시 적용이 되지 않는다.
                    XML에서 <context:load-time-weaver/> 시 정상 동작
                    JavaConfig에서 @EnableLoadTimeWeaving 지정시 동작하지 않음

--- src/main/java/com/apress/springrecipes/calculator/Main.java
+++ src/main/java/com/apress/springrecipes/calculator/Main.java
@@ -38,5 +38,6 @@ public class Main {
         ComplexCalculator complexCalculator = context.getBean("complexCalculator", ComplexCalculator.class);
         complexCalculator.add(new Complex(1, 2), new Complex(2,3));
         complexCalculator.sub(new Complex(5, 8), new Complex(2,3));
+
     }
 }
